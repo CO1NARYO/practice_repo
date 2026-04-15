1c180a372403751b5a09f05ba4143942e58d82ad
CO1N <155651132+CO1NARYO@users.noreply.github.com>
2026-04-15 18:09:05 +0800

    Add files via upload

---
+++ LoopPractice.java
@@ -0,0 +1,141 @@
+public class LoopPractice {
+    public static void main(String[] args){
+        
+        for(int i = 3; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            } 
+            System.out.println();
+        }  
+    }
+} 
+
+/* A.) 
+123
+12
+1 */ 
+
+
+/* public class LoopExercise { 
+    public static void main(String[] args){
+        
+        for(int i = 1; i <= 3; i++){
+            for(int j = 3; j >= i; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+} */
+
+/* B.) 
+321
+32
+3 */ 
+
+/* public class LoopExercise {
+    public static void main(String[] args){
+        
+        for(int i = 3; i >= 1; i--){
+            for(int j = i; j >= 1; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+} */
+
+/* C.)
+321
+21
+1 */
+
+
+/*public class LoopExercise {
+    public static void main(String[] args){ 
+        
+        for(int i = 3; i >= 1; i--){
+            for(int j = 3; j >= i; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+} */
+
+/* D.)
+3
+32
+321 */
+
+
+/*public class LoopExercise { 
+    public static void main(String[] args){
+        
+        for(int i = 1; i <= 3; i++){ 
+            for(int j = i; j <= 3; j++){
+                System.out.print(j);
+            }
+           System.out.println();
+        }
+    }
+} */ 
+
+/* E.)
+123 
+23
+3 */
+
+/*
+public class LoopPractice { 
+    public static void main(String[] args){
+        
+        for(int i = 3; i >= 1; i--){  /// mali dito 
+            for(int j = 1; j <= (4-i); j++){ // mali dito
+                System.out.print(i);
+            }
+                System.out.println();
+        }
+    }
+} */
+
+/* F.)
+3
+22
+111*/
+
+
+/* public class LoopPractice { 
+    public static void main(String[] args){
+        
+        for(int i = 3; i >= 1; i--){  /// mali dito 
+            for(int j = 1; j <= i; j++){ // mali dito
+                System.out.print(i);
+            }
+                System.out.println();
+        }
+    }
+} */
+
+/* G.) 
+333 
+22
+1 */
+
+
+/*public class LoopPractice { 
+    public static void main(String[] args){
+        
+        for(int i = 1; i <= 3; i++){  /// mali dito 
+            for(int j = 3; j >= i; j--){ // mali dito
+                System.out.print(i);
+            }
+                System.out.println();
+        }
+    }
+} */
+
+/* H.)
+111
+22
+3*/
